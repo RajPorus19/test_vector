9ba8813dadc6a53d8cf712f308084c152164ad1b
rajporus19 <raj.porus.hiruthayaraj@gmail.com>
2021-11-25 15:26:28 +0100

    set_vector test working

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 void test_vector_alloc(int* counter, p_s_vector vect);
 void test_insert_vector(int* counter, p_s_vector vect);
 void test_vector_erase(int* counter, p_s_vector vect);
+void test_vector_set(int* counter, p_s_vector vect);
+
+// TODO void vector_set(p_s_vector p_vector, size_t i, double v);
 
 int main(void){
 	int counter = 0;
@@ -13,6 +16,7 @@ int main(void){
 	test_vector_alloc(&counter, vect);
 	test_insert_vector(&counter, vect);
 	test_vector_erase(&counter, vect);
+	test_vector_set(&counter, vect);
 	// print result
 	if(counter==0) printf("All tests have passed successfuly ! :) \n");
 	else printf("%d tests have failed :(  \n",counter);
@@ -29,7 +33,7 @@ void test_vector_alloc(int* counter, p_s_vector vect){
 		if(vect->array[i] != 0) valid++;
 	}
 	if(valid!=1) {
-		printf("set_vector did not work as expected.\n");
+		printf("alloc_vector did not work as expected.\n");
 		*counter += 1;
 	}
 }
@@ -47,7 +51,7 @@ void test_insert_vector(int* counter, p_s_vector vect){
 	if(vect->array[0] != 0) valid++;
 	if(vect->array[1] != 10) valid++;
 	if(valid!=1) {
-		printf("set_vector did not work as expected.\n");
+		printf("insert_vector did not work as expected.\n");
 		*counter += 1;
 	}
 }
@@ -63,6 +67,24 @@ void test_vector_erase(int* counter, p_s_vector vect){
 	test_vector_alloc(counter, vect);
 	if(valid!=1) {
 		printf("vector_erase did not work as expected.\n");
+		*counter += 1;
+	}
+}
+
+
+void test_vector_set(int* counter, p_s_vector vect){
+	int valid = 1;
+	vector_set(vect, 0, 10);
+	// assert length is still 3
+	if(vect->length != 3) valid++;
+	// assert first element has been changed
+	if(vect->array[0] != 10) valid++;
+	// assert the remaining items are the same
+	for(size_t i = 1 ; i < vect->length ; i++){
+		if(vect->array[i] != 0) valid++;
+	}
+	if(valid!=1) {
+		printf("set_vector did not work as expected.\n");
 		*counter += 1;
 	}
 }
